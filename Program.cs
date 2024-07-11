8010d5e75774037059dd55f7e9962d393c1dff06
Roshani-Poojari <135858937+Roshani-Poojari@users.noreply.github.com>
2024-07-11 21:20:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineArgument
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numberArray = new double[5];
+            double sum, avg, max, min;
+
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                numberArray[i] = Convert.ToDouble(args[i]);
+            }
+
+            //summation
+            sum = numberArray.Sum();
+
+            //average
+            avg = sum / 5;
+
+            //maximum & minimum
+            max = numberArray.Max();
+            min = numberArray.Min();
+
+            Console.WriteLine("Sum: " + sum + "\nAverage: " + avg + "\nMaximum: " + max + "\nMinimum: " + min);
+
+        }
+
+
+    }
+}
